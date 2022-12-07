66aa4c17af21aa2c20e67bfa72140d9e9469abbd
Thomas Offergeld <34715927+TOffergeld@users.noreply.github.com>
2022-12-07 13:24:03 +0000

    Solution for day6, could refactor to use generics or at least generic queue length

---
+++ day6/Day6_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestSumPossessions(t *testing.T) {
+	want := 5
+	is := findPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")
+	if want != is {
+		t.Errorf("Wanted %d, got %d", want, is)
+	}
+}
+
+func TestSumPossessions2(t *testing.T) {
+	want := 10
+	is := findPacketMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+	if want != is {
+		t.Errorf("Wanted %d, got %d", want, is)
+	}
+}
